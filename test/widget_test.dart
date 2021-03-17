6b0cbe183408da65af9a8fa1c8fe903c3ed8efe3
viktor <viktor@snapp.mobile>
2021-03-17 15:54:38 +0200

    failed CI

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:my_flutter_app/main.dart';
+import 'package:my_flutter_app/main.dart';
+import 'package:my_flutter_app/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    // await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
     expect(find.text('10'), findsOneWidget);
